488dee0e3107d08b84b3c821da31c80e6babca46
MNMF <mmonsalve15@alumnos.utalca.cl>
2021-12-05 15:07:37 -0300

    contrato desplegado en rinkeby

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+
+const deploy = async () => {
+    const [deployer] = await ethers.getSigners();
+    console.log("deploying contract with the account:", deployer.address);
+
+    const VitalikPunks = await ethers.getContractFactory("VitalikPunks");
+    const deployed = await VitalikPunks.deploy();
+
+    console.log("Vitalik Punks is deployed at:", deployed.address);
+};  
+
+
+deploy () 
+    .then (() => process.exit(0))
+    .catch ((error) => {
+        console.log(error);
+        process.exit(1);
+    });
